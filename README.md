9bb2b536c26d097ba54dfac9156f6ecf45fbba87
Michelle Ross <miross629@gmail.com>
2022-10-31 14:49:06 -0700

    Emoji appears on correct guess and is removed for next guess

--- README.md
+++ README.md
@@ -5,6 +5,19 @@
 -   [x] Open PR from dev to main with your changes 0.5
 -   [x] Preview deploy from Netlify showing on your PR 0.5
 -   [x] On clicking a hiding place button, the total number of guesses increment 2
--   On clicking the correct hiding place button, the total number of correct guesses increment 2
--   On clicking the incorrect hiding place button, the number of incorrect guesses increments 2
+-   [x] On clicking the correct hiding place button, the total number of correct guesses increment 2
+-   [x] On clicking the incorrect hiding place button, the number of incorrect guesses increments 2
 -   On click, see the correct hiding place's image change, clearing out the previous correct guess style 3
+
+### Workflow
+
+-   Get emoji img
+-   Add ids to: tree, boulder, shed
+-   Add empty div below each img to hold the emoji img
+-   Style .hiding-img in CSS
+-   Apply .hiding-img to each button function within if..else statements:
+    -   tree.classList.add('hiding-img);
+
+## Attributions
+
+95C / 168 images. [_Alien Emoji._](https://pixabay.com/vectors/alien-emoji-happy-smiling-green-5858426/). Pixabay.
--- app.js
+++ app.js
@@ -20,12 +20,14 @@ let totalLosses = 0;
 
 treeButton.addEventListener('click', () => {
     totalGuesses++;
+    resetFace();
     const hidingSpot = Math.floor(Math.random() * 3);
     const answer = hidingPlaces[hidingSpot];
     handleGuess(answer, 'tree');
     if (answer === 'tree') {
         correctGuesses++;
         console.log(correctGuesses + 'W');
+        treeContainer.classList.add('face');
     } else {
         totalLosses++;
         console.log(totalLosses + 'L');
@@ -34,12 +36,14 @@ treeButton.addEventListener('click', () => {
 
 boulderButton.addEventListener('click', () => {
     totalGuesses++;
+    resetFace();
     const hidingSpot = Math.floor(Math.random() * 3);
     const answer = hidingPlaces[hidingSpot];
     handleGuess(answer, 'boulder');
     if (answer === 'boulder') {
         correctGuesses++;
         console.log(correctGuesses + 'W');
+        boulderContainer.classList.add('face');
     } else {
         totalLosses++;
         console.log(totalLosses + 'L');
@@ -48,17 +52,25 @@ boulderButton.addEventListener('click', () => {
 
 shedButton.addEventListener('click', () => {
     totalGuesses++;
+    resetFace();
     const hidingSpot = Math.floor(Math.random() * 3);
     const answer = hidingPlaces[hidingSpot];
     handleGuess(answer, 'shed');
     if (answer === 'shed') {
         correctGuesses++;
         console.log(totalGuesses + 'W');
+        shedContainer.classList.add('face');
     } else {
         totalLosses++;
         console.log(totalLosses + 'L');
     }
 });
+
+function resetFace() {
+    treeContainer.classList.remove('face');
+    boulderContainer.classList.remove('face');
+    shedContainer.classList.remove('face');
+}
 
 function handleGuess(correctSpot, userGuess) {
     // reset the styles
